5fa628249cd745ba4641196dae76457edd79bdd1
alberto1317 <huper16@gmail.com>
2026-03-13 19:32:24 -0600

    Agregar_ModeloCategoria

---
+++ Api_Peliculas/Modelos/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api_Peliculas.Modelos
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Nombre { get; set; }
+        [Required]
+        public DateTime FechaCreacion { get; set; }
+    }
+}
