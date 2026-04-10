ced0c05220a6004b51220039433af82e190836d0
alberto1317 <huper16@gmail.com>
2026-04-10 00:34:39 -0600

    AgregarCtegoriaPOST

--- Api_Peliculas/Controllers/CategoriasController.cs
+++ Api_Peliculas/Controllers/CategoriasController.cs
@@ -82,7 +82,30 @@ namespace Api_Peliculas.Controllers
                     return StatusCode(4004, ModelState);
                 }
                 return CreatedAtRoute("GetCategoria", new { categoriaId = categoria.Id }, categoria);
+        }
 
+        [HttpPatch("{categoriaId:int}", Name = "ActualizarPatchCategoria")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult ActualizarPatchCategoria(int categoriaId,[FromBody] CategoriaDto categoriaDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (categoriaDto == null || categoriaId != categoriaDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var categoria = _mapper.Map<Categoria>(categoriaDto);
+            if (!_ctRepo.ActualizarCategoria(categoria))
+            {
+                ModelState.AddModelError("", $"Algo salio mal actualizando el registro{categoria.Nombre}");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
 
         }
 
